aa47e5ea672adb05a0ff7a16706c8651c6285ae4
kjun <kjun_all@naver.com>
2023-06-07 01:47:59 +0900

    feat: protectedRouter 로직 수정

--- src/routers/Router.tsx
+++ src/routers/Router.tsx
@@ -1,22 +1,24 @@
-import { Route, Routes } from "react-router-dom";
-import Navbar from "../components/Layout";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Layout from "../components/Layout";
 import ProtectedRouter from "./ProtectedRouter";
 import MainPage from "../pages/MainPage";
 import SignUpPage from "../pages/SignUpPage";
 import SignInPage from "../pages/SignInPage";
 
-function Router() {
+function Routers() {
   return (
-    <Routes>
-      <Route path="/" element={<Navbar />}>
-        <Route element={<ProtectedRouter />}>
-          <Route path="/signup" element={<SignUpPage />}></Route>
-          <Route path="/signin" element={<SignInPage />}></Route>
-          <Route path="/todo" element={<MainPage />}></Route>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route element={<ProtectedRouter />}>
+            <Route path="/signup" element={<SignUpPage />}></Route>
+            <Route path="/signin" element={<SignInPage />}></Route>
+            <Route path="/todo" element={<MainPage />}></Route>
+          </Route>
         </Route>
-      </Route>
-    </Routes>
+      </Routes>
+    </Router>
   );
 }
 
-export default Router;
+export default Routers;
