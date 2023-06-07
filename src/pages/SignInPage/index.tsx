8afd9dab57a1b7ef92577635c34b22b0c5a42a0b
kjun <kjun_all@naver.com>
2023-06-08 04:55:13 +0900

    feat: 로그인 페이지 구현

--- src/pages/SignInPage/index.tsx
+++ src/pages/SignInPage/index.tsx
@@ -4,8 +4,8 @@ import AuthForm from "../../components/AuthForm";
 function SignInPage() {
   const navigate = useNavigate();
 
-  const loginNavigate = () => {
-    navigate("/signin");
+  const signUpNavigate = () => {
+    navigate("/signup");
   };
 
   return (
@@ -18,7 +18,7 @@ function SignInPage() {
 
         <div>
           <button
-            onClick={loginNavigate}
+            onClick={signUpNavigate}
             className="mt-4 flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             회원가입 페이지로 가기
