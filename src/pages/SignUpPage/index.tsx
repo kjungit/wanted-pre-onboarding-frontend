c8b266835eb3086afaa8bec26802aba230148a8c
kjun <kjun_all@naver.com>
2023-06-10 02:23:23 +0900

    feat: 로그인, 회원가입 alert 추가 및 수정

--- src/pages/SignUpPage/index.tsx
+++ src/pages/SignUpPage/index.tsx
@@ -3,12 +3,13 @@ import AuthForm from "../../components/AuthForm";
 
 function SignUpPage() {
   const navigate = useNavigate();
+
   const loginNavigate = () => {
     navigate("/signin");
   };
 
   return (
-    <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8 ">
+    <div className="flex min-h-full flex-col justify-center px-6 py-8 lg:px-8 ">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
           SignUp
@@ -18,7 +19,7 @@ function SignUpPage() {
         <div>
           <button
             onClick={loginNavigate}
-            className="mt-4 flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            className="mt-4 flex w-full justify-center rounded-md bg-black px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-600"
           >
             로그인 페이지로 가기
           </button>
